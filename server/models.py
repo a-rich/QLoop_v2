7142b1081882fd740e50872da6baf9d9b8800de9
Alex <alexrichards92@me.com>
2017-11-27 16:35:03 -0800

    API endpoints for account creation, recovery, and login

--- server/models.py
+++ server/models.py
@@ -1,10 +1,41 @@
 from __main__ import app
-from flask_mongoalchemy import MongoAlchemy
+from flask_mongoengine import MongoEngine
 
-db = MongoAlchemy(app)
+db = MongoEngine(app)
 
 class User(db.Document):
-    username = db.StringField(default="")
-    email = db.StringField(default="")
-    email_confirmed = db.BoolField(default=False)
-    password = db.StringField(default="")
+    username = db.StringField(default='')
+    email = db.EmailField()
+    password = db.StringField(default='')
+    profile_pic = db.ImageField(size=(400, 400, True))
+    favorite_songs_list = db.EmbeddedDocumentListField('Song')
+    friends_list = db.EmbeddedDocumentListField('User')
+
+    @staticmethod
+    def check_for_existing_user(email, username):
+        errors = {}
+
+        if User.objects(email=email):
+            errors['email'] = 'A user with this email has already created an account'
+        if User.objects(username=username):
+            errors['username'] = 'A user with this username has already created an account.'
+
+        return errors
+
+    @staticmethod
+    def add(email, username, password):
+        errors = {}
+
+        try:
+            User.objects.create(email=email, username=username, password=password)
+        except:
+            errors['save_user'] = 'Failed to save user document.'
+
+        return errors
+
+
+class Song(db.Document):
+    song_title = db.StringField(default='')
+    artist_name = db.StringField(default='')
+    url = db.URLField()
+    mp3 = db.BinaryField()
