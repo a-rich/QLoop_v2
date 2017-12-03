a4d6ee3ce1086ce6ea23208fab148ae6ef51c300
Alex <alexrichards92@me.com>
2017-12-03 13:01:04 -0800

    JWT?

--- server/app.py
+++ server/app.py
@@ -2,22 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt import JWT, jwt_required, current_identity
 
-def authenticate(username, password):
-    if username == user.username and password == user.password:
-        return user
-
-def identity(payload):
-    return user
-
 UPLOAD_FOLDER = 'static/imgs/'
 
 app = Flask(__name__)                                  # Actual Flask app.
-CORS(app)                                              # Cross origin requests
+CORS(app, resources=r"/api/*")
 app.config['SECRET_KEY'] = ':iw=PO5}H],oEtSa'          # Used for creating session.
 app.config['MONGOALCHEMY_DATABASE'] = 'database'       # Configure database name.
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-jwt = JWT(app, authenticate, identity)
 
 from views.myqloop import *
 from views.account import *
@@ -28,6 +19,19 @@ try:
     db.init_app(app)                                       # Initialize MongoAlchemy.
 except:
     pass
+
+def authenticate(username, password):
+    user = User.objects(username=username, password=password)
+    if user:
+        return user
+    else:
+        return False
+
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id": user_id}
+
+jwt = JWT(app, authenticate, identity)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
